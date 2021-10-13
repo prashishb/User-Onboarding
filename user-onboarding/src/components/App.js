fa011eb6d6906b01930720320cb36e6bb52027af
prashishb <prashb.web@gmail.com>
2021-10-13 14:50:28 -0400

    form, states created and basic css styling

--- user-onboarding/src/components/App.js
+++ user-onboarding/src/components/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './Form';
 
+// Initial Form State
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  tos: false,
+};
+
+// Initial Form Error States
+const InitialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+//Initial Submit Button State
+const initialDisabled = true;
+
 export default function App() {
+  // States
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(InitialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  // Event Handlers
+  const inputChange = (name, value) => {
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const formSubmit = () => {};
+
   return (
     <div>
       <h1>App</h1>
-      <Form />
+      <Form
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+      />
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,11 +1,66 @@
 import React from 'react';
 
-function Form() {
+export default function Form(props) {
+  const { values, change, submit, disabled, errors } = props;
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+
+  const onChange = (evt) => {
+    const { name, value, checked, type } = evt.target;
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse);
+  };
+
   return (
-    <div>
-      <p>Form</p>
-    </div>
+    <form className='form container' onSubmit={onSubmit}>
+      <div className='form-field'>
+        <label>
+          Name:
+          <input
+            type='text'
+            name='name'
+            value={values.name}
+            placeholder='John Doe'
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type='email'
+            name='email'
+            value={values.email}
+            placeholder='john@example.com'
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type='password'
+            name='password'
+            value={values.password}
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          Terms Of Service:
+          <input
+            type='checkbox'
+            name='tos'
+            className='tos'
+            onChange={onChange}
+          />
+        </label>
+        <div className='submit'>
+          <button type='submit' disabled={disabled}>
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
-
-export default Form;
