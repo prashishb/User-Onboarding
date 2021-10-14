67bf227489f8b158eee1481b069bad5af9fb3832
prashishb <prashb.web@gmail.com>
2021-10-14 14:29:53 -0400

    added test to check inputs and button status

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -27,4 +27,38 @@ describe('User Onboarding', () => {
     tosInput().should('exist');
     submitBtn().should('exist');
   });
+
+  describe('Filling out the inputs and check submit button status', () => {
+    // Submit Button should start out disabled
+    it('submit button starts out disabled', () => {
+      submitBtn().should('be.disabled');
+    });
+
+    // Type in the inputs
+    it('can type in the inputs', () => {
+      nameInput()
+        .should('have.value', '')
+        .type('Ethan Hunt')
+        .should('have.value', 'Ethan Hunt');
+
+      emailInput()
+        .should('have.value', '')
+        .type('ethan@mi.com')
+        .should('have.value', 'ethan@mi.com');
+
+      passInput()
+        .should('have.value', '')
+        .type('Verysecurypass444&')
+        .should('have.value', 'Verysecurypass444&');
+    });
+
+    it('can check box and select dropdown option', () => {
+      roleSelect()
+        .should('have.value', '')
+        .select('Project Manager')
+        .should('have.value', 'Project Manager');
+
+      tosInput().should('not.be.checked').check().should('be.checked');
+    });
+  });
 });
