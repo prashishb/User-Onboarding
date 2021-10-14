295b8f82699f4665cd05f7296913cc54e445dec6
prashishb <prashb.web@gmail.com>
2021-10-14 15:06:17 -0400

    MVP complete

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -60,5 +60,76 @@ describe('User Onboarding', () => {
 
       tosInput().should('not.be.checked').check().should('be.checked');
     });
+
+    // Submit button is enabled after filling out inputs and is added to the DOM
+    it('Submit button is enabled when form criteria meets', () => {
+      nameInput().type('Ethan Hunt');
+      emailInput().type('ethan@mi.com');
+      passInput().type('Verysecurypass444&');
+      roleSelect().select('Project Manager');
+      tosInput().check();
+      submitBtn().should('be.enabled');
+    });
+  });
+
+  describe('form data is submitted and is displayed on DOM', () => {
+    it('Form Data can be submitted', () => {
+      nameInput().type('Ethan Hunt');
+      emailInput().type('ethan@mi.com');
+      passInput().type('Verysecurypass444&');
+      roleSelect().select('Project Manager');
+      tosInput().check();
+      submitBtn().should('be.enabled');
+      submitBtn().click();
+    });
+
+    it('form data is displayed on DOM', () => {
+      nameInput().type('Ethan Hunt');
+      emailInput().type('ethan@mi.com');
+      passInput().type('Verysecurypass444&');
+      roleSelect().select('Project Manager');
+      tosInput().check();
+      submitBtn().should('be.enabled');
+      submitBtn().click();
+      cy.contains(/ethan hunt/i).should('exist');
+      cy.contains('ethan@mi.com').should('exist');
+      cy.contains('Verysecurypass444&').should('exist');
+      cy.contains(/project manager/i).should('exist');
+      cy.contains('✔️').should('exist');
+    });
+  });
+
+  describe('form validation if input is left empty', () => {
+    it('button disabled if empty name', () => {
+      emailInput().type('bob@bob.com');
+      passInput().type('Verysecurypass444');
+      roleSelect().select('Project Manager');
+      tosInput().check();
+      submitBtn().should('be.disabled');
+    });
+
+    it('button disabled if empty email', () => {
+      nameInput().type('Bob');
+      passInput().type('Verysecurypass444');
+      roleSelect().select('Project Manager');
+      tosInput().check();
+      submitBtn().should('be.disabled');
+    });
+
+    it('button disabled if empty password', () => {
+      nameInput().type('Bob');
+      emailInput().type('bob@bob.com');
+      roleSelect().select('Project Manager');
+      tosInput().check();
+      submitBtn().should('be.disabled');
+    });
+
+    it('button disabled if role not selected', () => {
+      nameInput().type('Bob');
+      emailInput().type('bob@bob.com');
+      passInput().type('Verysecurypass444');
+      tosInput().check();
+      submitBtn().should('be.disabled');
+    });
   });
 });
